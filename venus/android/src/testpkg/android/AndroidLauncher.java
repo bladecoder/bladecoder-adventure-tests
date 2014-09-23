4f4c6183aed5d6c1f1f08e1176dc39c9afc5c076
rgarcia <bladecoder@gmail.com>
2014-09-23 15:50:34 +0200

    Gradle conversion STEP 1

---
+++ venus/android/src/testpkg/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package testpkg.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import testpkg.testMainClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new testMainClass(), config);
+	}
+}

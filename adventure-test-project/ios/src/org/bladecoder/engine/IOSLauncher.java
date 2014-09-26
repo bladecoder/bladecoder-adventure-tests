976ac686540d17f7ba947072e1bb510ad61f504d
rgarcia <bladecoder@gmail.com>
2014-09-26 10:12:06 +0200

    gradle layout

---
+++ adventure-test-project/ios/src/org/bladecoder/engine/IOSLauncher.java
@@ -0,0 +1,22 @@
+package org.bladecoder.engine;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import org.bladecoder.engine.BladeEngine;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new BladeEngine(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}

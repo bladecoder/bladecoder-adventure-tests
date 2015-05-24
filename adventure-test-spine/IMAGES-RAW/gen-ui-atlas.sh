be6b4b1ee7f222669711be23fb0bdf3cb5f224c3
bladecoder <bladecoder@gmail.com>
2015-05-25 00:32:40 +0200

    added ui raw images

---
+++ adventure-test-spine/IMAGES-RAW/gen-ui-atlas.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+VERSION=1.6.0
+LIBGDX_BASE_PATH=~/.gradle/caches/modules-2/files-2.1/com.badlogicgames.gdx/
+GDX_PATH=`find $LIBGDX_BASE_PATH -name gdx-$VERSION.jar`
+GDX_TOOLS_PATH=`find $LIBGDX_BASE_PATH -name gdx-tools-$VERSION.jar`
+OUT_DIR=../android/assets/ui/1/
+
+cp pack.json ui
+java -cp $GDX_PATH:$GDX_TOOLS_PATH com.badlogic.gdx.tools.texturepacker.TexturePacker ui $OUT_DIR ui

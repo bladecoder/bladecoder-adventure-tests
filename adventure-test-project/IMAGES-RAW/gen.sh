6b215497092bf4da7b9e1aff75a8b85e1ba45cf2
bladecoder <bladecoder@gmail.com>
2016-01-02 11:38:39 +0100

    parallax scene test.

--- adventure-test-project/IMAGES-RAW/gen.sh
+++ adventure-test-project/IMAGES-RAW/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSION=1.6.4
+VERSION=1.6.5
 LIBGDX_BASE_PATH=~/.gradle/caches/modules-2/files-2.1/com.badlogicgames.gdx/
 GDX_PATH=`find $LIBGDX_BASE_PATH -name gdx-$VERSION.jar`
 GDX_TOOLS_PATH=`find $LIBGDX_BASE_PATH -name gdx-tools-$VERSION.jar`
@@ -11,5 +11,7 @@ if [ $# -eq 0 ]
     exit
 fi
 
-cp pack.json $1
-java -cp $GDX_PATH:$GDX_TOOLS_PATH com.badlogic.gdx.tools.texturepacker.TexturePacker $1 $OUT_DIR $1
+target=${1%/}
+
+cp pack.json $target
+java -cp $GDX_PATH:$GDX_TOOLS_PATH com.badlogic.gdx.tools.texturepacker.TexturePacker $target $OUT_DIR $target

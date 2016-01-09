24d83682e17a54d3f761dc9e46a4cbdd72625ccf
bladecoder <bladecoder@gmail.com>
2016-01-09 10:01:09 +0100

    update ui atlas

--- venus/IMAGES-RAW/gen-ui-atlas.sh
+++ venus/IMAGES-RAW/gen-ui-atlas.sh
@@ -5,5 +5,5 @@ GDX_PATH=`find $LIBGDX_BASE_PATH -name gdx-$VERSION.jar`
 GDX_TOOLS_PATH=`find $LIBGDX_BASE_PATH -name gdx-tools-$VERSION.jar`
 OUT_DIR=../android/assets/ui/1/
 
-cp pack.json ui
+#cp pack.json ui
 java -cp $GDX_PATH:$GDX_TOOLS_PATH com.badlogic.gdx.tools.texturepacker.TexturePacker ui $OUT_DIR ui

fba177d18737d150b967b7203d255fa6e866bd12
bladecoder <bladecoder@gmail.com>
2017-10-29 21:51:25 +0100

    Updated Blade Engine to v1.3.2

---
+++ adventure-test-project/android/src/main/java/com/bladecoder/advtest/AndroidLauncher.java
@@ -0,0 +1,26 @@
+package com.bladecoder.advtest;
+
+import com.bladecoder.engine.BladeEngine;
+
+import android.os.Bundle; 
+import android.view.WindowManager;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useAccelerometer = false;
+        cfg.useCompass = false;
+        //cfg.numSamples = 2;
+        cfg.useImmersiveMode = true;
+        
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        
+        initialize(new BladeEngine(), cfg);
+    }
+}
